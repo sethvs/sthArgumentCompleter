59564a6ba99dd0c5e03251c95960a91573ad2fae
sethvs <sethvs@users.noreply.github.com>
2019-03-29 12:40:48 +0300

    Clean.

--- sthArgumentCompleterCompleters.ps1
+++ sthArgumentCompleterCompleters.ps1
@@ -52,30 +52,3 @@ class sthArgumentCompleter : System.Management.Automation.IArgumentCompleter
         return $result
     }
 }
-<# 
-class sthNativeArgumentCompleter : System.Management.Automation.IArgumentCompleter
-{
-    [System.Collections.Generic.IEnumerable[System.Management.Automation.CompletionResult]] CompleteArgument(
-        [string] $commandName,
-        [string] $parameterName,
-        [string] $wordToComplete,
-        [System.Management.Automation.Language.CommandAst] $commandAst,
-        [System.Collections.IDictionary] $fakeBoundParameters
-    )
-    {
-        $result = New-Object -TypeName "System.Collections.Generic.List[System.Management.Automation.CompletionResult]"
-
-        $argumentCompleters = inGetArgumentCompleter -Type Native
-
-        foreach ($key in $argumentCompleters.Keys)
-        {
-            if ($key -like "$wordToComplete*")
-            {
-                $result.Add([System.Management.Automation.CompletionResult]::new($key))
-            }
-        }
-
-        return $result
-    }
-}
- #>
